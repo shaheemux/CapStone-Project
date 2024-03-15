1793b771172257e031850378015d1f7e5ea2bfd4
unknown <Shaheemsalie349@gmail.com>
2024-03-15 13:00:00 -0400

    added files

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -8,8 +8,8 @@
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
         <div class="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
-            <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a>
+            <routerlink to="/login"  class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</routerlink>
+            <routerlink to="/login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</routerlink>
             <button data-collapse-toggle="mega-menu" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -32,77 +32,77 @@
                         <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
                             <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="/about" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         About Us
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Library
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Resources
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Pro Version
-                                    </a>
+                                    </router-link>
                                 </li>
                             </ul>
                         </div>
                         <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
                             <ul class="space-y-4">
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to=""class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Blog
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Newsletter
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Playground
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         License
-                                    </a>
+                                    </router-link>
                                 </li>
                             </ul>
                         </div>
                         <div class="p-4">
                             <ul class="space-y-4">
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Contact Us
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Support Center
-                                    </a>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                    <router-link to="" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                         Terms
-                                    </a>
+                                    </router-link>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</a>
+                    <router-link to="" class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</router-link>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                    <a to="" class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                 </li>
             </ul>
         </div>
--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,31 +1,34 @@
 <template>
-    <div>
-       <h1>Login Page</h1>
-      <input type="email" placeholder="username" name="Email_add" required v-model="username"><br><br>
-      <input type="password" placeholder="password" name="User_password" required v-model="password">
-      <br>
-      <br>
-      {{$store.state.loggedIn}}
-      <button @click="loginUser()">Login</button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-       data(){
-              return{
-                  Email_add:'',
-                  User_password:''
-              }
-          },
-          methods:{
-              loginUser(){
-                 this.$store.dispatch('checkPassword',this.$data)  
-              }
-          }
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+    <form @submit.prevent="login">
+       <input v-model="email" type="email" placeholder="Email" required />
+       <input v-model="password" type="password" placeholder="Password" required />
+       <button type="submit">Login</button>
+    </form>
+   </template>
+   
+   <script>
+   import { mapActions } from 'vuex';
+   
+   export default {
+    data() {
+       return {
+         email: '',
+         password: '',
+       };
+    },
+    methods: {
+       ...mapActions('auth', ['login']),
+       async login() {
+         try {
+           await this.login({ email: this.email, password: this.password });
+           // Redirect to home page or dashboard after successful login
+           this.$router.push('/');
+         } catch (error) {
+           console.error(error);
+           // Handle error (e.g., display error message)
+         }
+       },
+    },
+   };
+   </script>
+   
