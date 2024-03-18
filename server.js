e15de6b7b3714a06b83e0ef7f73c02f6c5fda77c
shaheemux <120124594+shaheemux@users.noreply.github.com>
2024-03-18 16:19:17 +0200

    added files

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ const app = express()
 const port = +process.env.MYSQL_ADDON_PORT || 3001
 
 
-app.use(cors({
-    origin: 'https://localhost:8080',
-    Credentials: true
-}))
+app.use(cors())
+
+
 app.use(express.json())
 app.use(express.static( 'public' ))
 app.use('/products', productsRoute)
